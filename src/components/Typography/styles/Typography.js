b1ddf517fe390eafc541409366ba4d0407d34e08
Nunes <nicolas.mgta+git@gmail.com>
2020-10-13 00:39:33 +0200

    Typography compponent

---
+++ src/components/Typography/styles/Typography.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const TitleLarge = styled.h1`
+    font-size: 13.5rem;
+    font-weight: 800;
+    margin-bottom: 13px;
+    letter-spacing: -3.6px;
+
+    @media(max-width: 1056px){
+        font-size: 9.5rem;
+        margin-bottom: 23px;
+    }
+`;
+export const TitleMd = styled.h2`
+    font-size: 5.6rem;
+    font-weight: 800;
+    letter-spacing: -1.9px;
+    letter-spacing: -1.9px;
+
+    @media(max-width: 672px){
+        font-size: 2.3rem;
+    }
+`;
+export const TitleSm = styled.h3`
+    font-size: 3.6rem;
+    margin-bottom: 23px;
+    @media(max-width: 672px){
+        font-size: 2.3rem;
+    }
+`;
+export const TitleXs = styled.h4`
+    font-size: 1.9rem;
+    font-weight: 800;
+    margin-bottom: 13px;
+`;
+export const TitleXxs = styled.h5`
+    font-size: 1.9rem;
+`;
+export const TitleH6 = styled.h6`
+    font-size: 1.9rem;
+`;
+export const BodyLarge = styled.p`
+    font-size: 2.3rem;
+    margin-bottom: 23px;
+`;
+export const Body = styled.p`
+    font-size: 1.9rem;
+    font-weight: 400;
+    color: #D8D8D8;
+    opacity: .6;
+`;
+export const BodySmall = styled.p`
+    font-size: 1.3rem;
+`;
