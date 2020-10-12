cbbdbab882c3aa1487dec4831eb7b06c9bb26bf3
Nunes <nicolas.mgta+git@gmail.com>
2020-10-13 00:40:25 +0200

    export components

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Typography } from "./Typography/index";
+export { default as GridLayout } from "./GridLayout/index";
---
+++ src/containers/index.js
@@ -0,0 +1,1 @@
+export { default as GridLayoutContainer } from './GridLayoutContainer';
