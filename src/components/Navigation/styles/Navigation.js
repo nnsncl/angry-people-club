6e9abe137e9fc75b5a91fc9564a41b2e593f686b
Nunes <nicolas.mgta+git@gmail.com>
2020-10-13 23:28:37 +0200

    Navigation component

---
+++ src/components/Navigation/styles/Navigation.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.nav`
+    padding: 2.3rem;
+`;
+
+export const Frame = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const Item = styled.div`
+`;
+
+
+export const Logotype = styled.a`
+    font-size: 2.3rem;
+    cursor: pointer;
+    font-family: 'Germania One', cursive;
+    color: #F4DE99;
+`;
