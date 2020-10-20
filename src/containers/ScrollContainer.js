6ce78991e3c4bf0524b49a9ada7a97fb08fecbd3
Nunes <nicolas.mgta+git@gmail.com>
2020-10-20 21:51:32 +0200

    ScrollContainer comments

--- src/containers/ScrollContainer.js
+++ src/containers/ScrollContainer.js
@@ -8,7 +8,7 @@ import {
   motion
 } from "framer-motion"
 
-
+// Empty div with dynamic height
 const Container = styled(motion.div)`
     position: fixed;
     top: 0;
@@ -19,20 +19,20 @@ const Container = styled(motion.div)`
 `;
 
 export default function ScrollContainer({ children }) {
-  // scroll container
+  // ScrollContainer
   const scrollRef = useRef(null)
 
-  // page scrollable height based on content length
+  // Page scrollable height based on content length
   const [pageHeight, setPageHeight] = useState(0)
 
-  // update scrollable height when browser is resizing
+  // Update scrollable height when browser is resizing
   const resizePageHeight = useCallback(entries => {
     for (let entry of entries) {
       setPageHeight(entry.contentRect.height)
     }
   }, [])
 
-  // observe when browser is resizing
+  // Browser size observer
   useLayoutEffect(() => {
     const resizeObserver = new ResizeObserver(entries =>
       resizePageHeight(entries)
@@ -41,22 +41,23 @@ export default function ScrollContainer({ children }) {
     return () => resizeObserver.disconnect()
   }, [scrollRef, resizePageHeight])
 
-  const { scrollY } = useViewportScroll() // measures how many pixels user has scrolled vertically
-  // as scrollY changes between 0px and the scrollable height, create a negative scroll value...
-  // ... based on current scroll position to translateY the document in a natural way
+  const { scrollY } = useViewportScroll()
   const transform = useTransform(scrollY, [0, pageHeight], [0, -pageHeight])
-  const physics = { damping: 13, mass: .9, stiffness: 56 } // easing of smooth scroll
-  const spring = useSpring(transform, physics) // apply easing to the negative scroll value
+  // Scroll Easing
+  const physics = { damping: 13, mass: .9, stiffness: 56 }
+  const spring = useSpring(transform, physics)
 
   return (
     <>
       <Container
         ref={scrollRef}
-        style={{ y: spring }} // translateY of scroll container using negative scroll value
+         // TranslateY of ScrollContainer using negative scroll value
+        style={{ y: spring }}
       >
         {children}
       </Container>
-      {/* blank div that has a dynamic height based on the content's inherent height this is neccessary to allow the scroll container to scroll using the browser's native scroll bar  */}
+      {/* Empty div that has a dynamic height based on the content's inherent height.
+      Mandatory to allow the ScrollContainer to scroll using the browser's native scroll bar. */}
       <div style={{ height: pageHeight }} />
     </>
   )
