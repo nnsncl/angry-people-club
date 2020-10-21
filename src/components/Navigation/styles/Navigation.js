7a81276c1d8825801e85b3aa0a8502a4d6b6900b
Nunes <nicolas.mgta+git@gmail.com>
2020-10-21 21:58:32 +0200

    Navigation adjustments

--- src/components/Navigation/styles/Navigation.js
+++ src/components/Navigation/styles/Navigation.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Link as ReachRouterLink } from 'react-router-dom'
 
 export const Container = styled.nav`
     padding: 2.3rem;
@@ -11,13 +12,29 @@ export const Frame = styled.div`
 `;
 
 export const Item = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    p {
+        margin-left: .9rem;
+    }
 `;
 
 
-export const Logotype = styled.a`
-    font-size: 3.6rem;
+export const Logotype = styled(ReachRouterLink)`
+    ${'' /* font-size: 3.6rem;
     line-height: .9em;
     letter-spacing: -1.3px;
     cursor: pointer;
-    font-family: 'Germania One', cursive;
+    font-family: 'Germania One', cursive; */}
+    appearance: button;
+    -moz-appearance: button;
+    -webkit-appearance: button;
+    ${'' /* padding: 2.3rem;
+    border-radius: 2rem;
+    background: white; */}
+    cursor: pointer;
+    transition: ease-out .2s;
+    text-decoration: none;
 `;
