2385feb94265764b418face85f3668506f2aa1ae
Nunes <nicolas.mgta+git@gmail.com>
2020-10-22 23:40:07 +0200

    Create footer compoent

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Typography } from "./Typography/index";
 export { default as GridLayout } from "./GridLayout/index";
 export { default as Navigation } from './Navigation/index';
 export { default as Button } from './Button/index';
+export { default as Footer } from './Footer/index';
