66b4e1ee180c333bf9fd0d83d2e2a7d3385f0562
Nunes <nicolas.mgta+git@gmail.com>
2020-11-03 00:52:34 +0100

    refactor contexts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Home, SignIn, SignUp, Landing } from './pages';
-import { AuthProvider } from './auth/Auth';
 import PrivateRoute from './utils/PrivateRoute';
-import ScrollContainer from './utils/ScrollContainer';
+import { AnimatePresence } from 'framer-motion';
 
 export default function App() {
+
   return (
-    <AuthProvider>
-      <Router>
-        <ScrollContainer>
+    <>
+    <Router>
+      <AnimatePresence>
+        <Switch>
           <PrivateRoute exact path='/' component={Home} />
           <Route exact path='/sabotage' component={Landing} />
           <Route exact path='/signin' component={SignIn} />
           <Route exact path='/signup' component={SignUp} />
-        </ScrollContainer>
-      </Router>
-    </AuthProvider>
+        </Switch>
+      </AnimatePresence>
+    </Router>
+    </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { AuthProvider } from './auth/Auth';
 import { GlobalStyles } from './theme/global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
