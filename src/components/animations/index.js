e7893a6c745e4d5ec58a637674d225b9fff1e620
Nunes <nicolas.mgta+git@gmail.com>
2020-11-03 20:52:26 +0100

    create animation variable

---
+++ src/components/animations/index.js
@@ -0,0 +1,13 @@
+export const layoutVariant = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: { duration: 0.3 },
+    },
+    exit: {
+        opacity: 0,
+        transition: { duration: 0.3 },
+    },
+};
