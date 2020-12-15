3050bf43ebadcdf876396111b1ddcf3db9b2c568
nnsncl <nicolas.mgta@gmail.com>
2020-12-15 20:59:11 +0100

    remove prev page from the DOM when redirect

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,25 @@
 import React, { useContext } from 'react';
+
 import firebaseApp from '../services/firebase';
 import { AuthContext } from '../auth/Auth';
 
 import { motion } from 'framer-motion';
 import { layoutVariant } from '../theme/animations/index'
+
+import ScrollContainer from '../utils/ScrollContainer';
+
+import { Typography, AnimatedTitle, Button } from '../components';
+import { Layout } from '../components/layout/index';
+
+import {
+    GridLayoutContainer,
+    NavigationContainer,
+    DirectionalScrollContainer,
+    FooterContainer,
+} from '../containers';
+
+
+
 
 export default function Home() {
 
@@ -17,9 +33,20 @@ export default function Home() {
             animate="visible"
             exit="exit"
         >
-            <h1>Home</h1>
-            <button onClick={() => firebaseApp.auth().signOut()} >Sign out</button>
-            {/* Call signOut from auth module to log out the user on click using the firebase API */}
+            <ScrollContainer>
+                <NavigationContainer hasNavigation />
+                <Layout>
+                    <Layout.Row  >
+                        <Layout.Col size={'3'} >
+                        <AnimatedTitle>Let's start by your user name</AnimatedTitle>
+                        </Layout.Col>
+                        <Layout.Col size={'1'} style={{backgroundColor: 'red'}}>
+                            <p>a</p>
+                        </Layout.Col>
+                    </Layout.Row>
+                </Layout>
+                <FooterContainer />
+            </ScrollContainer>
         </motion.div>
     );
 };
