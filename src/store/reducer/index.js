51c7c8da90147e48f7531d8553af25cfb20731ce
nnsncl <nicolas.mgta@gmail.com>
2020-12-20 22:45:16 +0100

    Create redux actions to handle ongoing and incoming messages

---
+++ src/store/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { ProcessReducer } from './process';
+
+const rootReducers = combineReducers({
+    ProcessReducer: ProcessReducer,
+});
+
+export default rootReducers;
