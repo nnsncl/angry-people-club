414e2acf05dd7556cd6c05b36799fe693cdbe6e8
nnsncl <nicolas.mgta@gmail.com>
2020-12-20 22:38:27 +0100

    edit index to include store provider (might change)

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducers from './store/reducer/index';
+
 import { GlobalStyles } from './styles/global-styles';
+
+const store = createStore(rootReducers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <Provider store={store} >
+      <GlobalStyles />
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
