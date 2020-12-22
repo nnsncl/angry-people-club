97e29e22efbc7fd07533bbb896980bd62e0c5302
nnsncl <nicolas.mgta@gmail.com>
2020-12-22 16:56:06 +0100

    chat container base

---
+++ src/containers/ChatContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Sidebar, Avatar, Typography, Button } from '../components';
+
+
+export default function ChatContainer(){
+    return (
+        <div style={{ width: "100%", background: 'red' }} >
+a
+        </div>
+    );
+};
