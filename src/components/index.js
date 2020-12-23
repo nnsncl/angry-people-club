ec213cdeae2279338060fad68890a5b243fd490d
nnsncl <nicolas.mgta@gmail.com>
2020-12-23 21:55:39 +0100

    create chatbrelated components folders

--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,7 @@ export { default as Navigation } from './Navigation/index';
 export { default as Sidebar } from './Sidebar/index';
 export { default as Avatar } from './Avatar/index';
 export { default as Form } from './Form/index';
-export { default as Input } from './Input/index';
+export { default as ChatInput } from './ChatInput/index';
 export { default as Button } from './Button/index';
 export { default as Footer } from './Footer/index';
 export * from './AnimatedTitle/index'
