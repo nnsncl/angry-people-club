0857bdf3ace90cdc0c4756b35c90b3cddca599eb
nnsncl <nicolas.mgta@gmail.com>
2020-12-23 23:04:24 +0100

    use appbar component

--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Sidebar, Avatar, Typography, Button } from '../components';
+// import styled from 'styled-components';
+import { Typography, Button, AppBar } from '../components';
 
 
-export default function ChatContainer(){
+export default function ChatContainer() {
     return (
-        <div style={{ width: "100%", background: 'red' }} >
-            Chat screen
+        <div style={{ display: "flex", flex: "1", flexDirection: 'column' }}>
+            <AppBar>
+                <Typography.TitleXs><b>#</b>&nbsp;Room name</Typography.TitleXs>
+                <AppBar.Frame>
+                    <Button.Ghost>&#9656;</Button.Ghost>
+                    <Button.Ghost>&#9656;</Button.Ghost>
+                    <Button.Ghost>Leave the room</Button.Ghost>
+                </AppBar.Frame>
+            </AppBar>
+            <div style={{ flex: "1" }} >
+                Chat
+            </div>
+            <div style={{ background: '#212121', padding: '5.6rem', margin: '2.3rem', borderRadius: "8px" }} >
+                Input
+            </div>
         </div>
     );
 };
