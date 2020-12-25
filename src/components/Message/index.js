66955197f56f9eac144f6c297d6de4bc9faf02b9
nnsncl <nicolas.mgta@gmail.com>
2020-12-26 00:57:39 +0100

    add props to message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { Wrapper, Frame, Heading, Content } from './styles/Message';
 import { Avatar, Typography } from '../../components';
 
-import { useAuth } from '../../hooks/use-auth';
-
-export default function Message() {
-    const auth = useAuth();
+export default function Message({ backgroundURL, userName, timeStamp, content }) {
 
     return (
         <Wrapper>
             <Frame>
-                <Avatar Xlarge backgroundURL={auth.user.photoURL} />
+                <Avatar Xlarge backgroundURL={backgroundURL} />
             </Frame>
             <Frame>
                 <Heading>
-                    <Typography.Body><b>{auth.user.displayName}</b>&nbsp;•&nbsp;</Typography.Body>
-                    <Typography.BodySmall>Sent the 12/12/2012</Typography.BodySmall>
+                    <Typography.Body><b>{userName}</b>&nbsp;•&nbsp;</Typography.Body>
+                    <Typography.BodySmall>Sent the&nbsp;{timeStamp}</Typography.BodySmall>
                 </Heading>
                 <Content>
-                    <Typography.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Typography.Body>
+                    <Typography.Body>{content}</Typography.Body>
                 </Content>
             </Frame>
         </Wrapper>
--- src/containers/ChatContainer.js
+++ src/containers/ChatContainer.js
@@ -1,24 +1,42 @@
 import React from 'react';
-// import styled from 'styled-components';
-import { Message } from '../components';
+
+import { useAuth } from '../hooks/use-auth';
+
+import { Message, ChatInput } from '../components';
 import { Layout } from '../components/layout/index';
 
 
 export default function ChatContainer() {
+    const auth = useAuth();
     return (
         <>
             <div style={{ flex: "1" }} >
                 <Layout maxFreeze >
                     <Layout.Row>
                         <Layout.Col size="1" >
-                            <Message />
+                            <Message
+                                backgroundURL={auth.user.photoURL}
+                                userName={auth.user.displayName}
+                                timeStamp='12/12/2012'
+                                content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                            />
+                            <Message
+                                backgroundURL={auth.user.photoURL}
+                                userName={auth.user.displayName}
+                                timeStamp='12/12/2012'
+                                content='Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                            />
+                            <Message
+                                backgroundURL={auth.user.photoURL}
+                                userName={auth.user.displayName}
+                                timeStamp='12/12/2012'
+                                content='Fok Niki dolor sit amet,'
+                            />
                         </Layout.Col>
                     </Layout.Row>
                 </Layout>
             </div>
-            <div style={{ background: '#212121', padding: '5.6rem', margin: '2.3rem', borderRadius: "8px" }} >
-                Input
-                </div>
+            <ChatInput />
         </>
     );
 };
