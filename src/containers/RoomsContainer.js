9dd8998ed3f5cad644e2e38f3ced2b0d1cfd61cb
nnsncl <nicolas.mgta@gmail.com>
2020-12-25 23:43:28 +0100

    change icons

--- src/containers/RoomsContainer.js
+++ src/containers/RoomsContainer.js
@@ -9,21 +9,36 @@ export default function MembersContainer({ id, room }) {
         <>
             <Sidebar.Label>
                 <Typography.BodySmall>Rooms</Typography.BodySmall>
-                <Button.Ghost>&#43;</Button.Ghost>
+                <Button.Ghost>
+                    <svg width="24" height="24" viewBox="0 0 48 48" fill="none" >
+                        <rect x="8" y="22" width="32" height="6" rx="1.5" fill="white" />
+                        <rect opacity="0.6" x="26" y="8" width="32" height="6" rx="1.5" transform="rotate(90 26 8)" fill="white" />
+                    </svg>
+                </Button.Ghost>
             </Sidebar.Label>
             <Sidebar.Item hasSmallMarginBottom flexItem >
                 <Sidebar.Item>
                     <Avatar large backgroundURL='https://media.giphy.com/media/Lopx9eUi34rbq/giphy.gif' />
                     <Typography.Body>Hell's Kitchen</Typography.Body>
                 </Sidebar.Item>
-                <Button.Ghost>&#9656;</Button.Ghost>
+                <Button.Ghost>
+                    <svg width="24" height="24" viewBox="0 0 48 48" fill="none">
+                        <rect opacity="0.6" x="10" y="26" width="6" height="28" rx="1" transform="rotate(-90 10 26)" fill="white" />
+                        <path d="M22.5858 34.5858C21.8047 35.3668 21.8047 36.6332 22.5858 37.4142C23.3668 38.1953 24.6332 38.1953 25.4142 37.4142L37.4142 25.4142C38.1714 24.6571 38.1979 23.4379 37.4743 22.6485L26.4743 10.6485C25.7279 9.83431 24.4628 9.7793 23.6486 10.5257C22.8343 11.2721 22.7793 12.5372 23.5257 13.3514L33.2317 23.9398L22.5858 34.5858Z" fill="white" />
+                    </svg>
+                </Button.Ghost>
             </Sidebar.Item>
             <Sidebar.Item hasMarginBottom flexItem >
                 <Sidebar.Item>
                     <Avatar large backgroundURL='https://media.giphy.com/media/2U1WOzgmZVw2Y/giphy.gif' />
                     <Typography.Body>Los Moskitos</Typography.Body>
                 </Sidebar.Item>
-                <Button.Ghost>&#9656;</Button.Ghost>
+                <Button.Ghost>
+                    <svg width="24" height="24" viewBox="0 0 48 48" fill="none">
+                        <rect opacity="0.6" x="10" y="26" width="6" height="28" rx="1" transform="rotate(-90 10 26)" fill="white" />
+                        <path d="M22.5858 34.5858C21.8047 35.3668 21.8047 36.6332 22.5858 37.4142C23.3668 38.1953 24.6332 38.1953 25.4142 37.4142L37.4142 25.4142C38.1714 24.6571 38.1979 23.4379 37.4743 22.6485L26.4743 10.6485C25.7279 9.83431 24.4628 9.7793 23.6486 10.5257C22.8343 11.2721 22.7793 12.5372 23.5257 13.3514L33.2317 23.9398L22.5858 34.5858Z" fill="white" />
+                    </svg>
+                </Button.Ghost>
             </Sidebar.Item>
         </>
     );
--- src/containers/SidebarContainer.js
+++ src/containers/SidebarContainer.js
@@ -30,7 +30,13 @@ export default function SidebarContainer() {
                     <Typography.BodySmall>Current Room</Typography.BodySmall>
                 </Sidebar.Label>
                 <Sidebar.Item hasBackground>
-                    <Typography.Body><b>#</b>&nbsp;&nbsp;Hell's Kitchen</Typography.Body>
+                    <Typography.Body>
+                    <span>
+                        <svg width="16" height="16" viewBox="0 0 48 48" fill="none">
+                            <path opacity="0.3" fill-rule="evenodd" clip-rule="evenodd" d="M10 30L6 43L19 39L10 30Z" fill="white" />
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M16 4C10.4772 4 6 8.47705 6 14V34C6 39.5229 10.4772 44 16 44H36C41.5228 44 46 39.5229 46 34V14C46 8.47705 41.5228 4 36 4H16ZM14.8906 24.3359C13.9716 24.9487 13.7231 26.1904 14.3358 27.1094C17.3551 31.6387 21.2913 34 26 34C30.7086 34 34.6448 31.6387 37.6641 27.1094C38.2767 26.1904 38.0284 24.9487 37.1094 24.3359C36.1903 23.7236 34.9486 23.9717 34.3358 24.8906C32.0219 28.3618 29.2913 30 26 30C22.7086 30 19.9781 28.3618 17.6641 24.8906C17.0514 23.9717 15.8096 23.7236 14.8906 24.3359Z" fill="white" />
+                        </svg>
+                    </span>&nbsp;&nbsp;Hell's Kitchen</Typography.Body>
                 </Sidebar.Item>
             </Sidebar.Header>
 
