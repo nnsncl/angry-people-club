e8ce5d9e52ac88cfe64af55bd749dab5738ba59b
nnsncl <nicolas.mgta@gmail.com>
2020-12-26 04:34:49 +0100

    add dispatch dep to privateRoute useEffect

--- src/hooks/use-private-route.js
+++ src/hooks/use-private-route.js
@@ -31,7 +31,7 @@ export default function PrivateRoute({ component: RouteComponent, ...restProps }
           }
         })
       // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, [])
+      }, [dispatch])
       
     return (
         <Route {...restProps}
