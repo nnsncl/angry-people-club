0ec5fba354cb9fe4814de38afe668d77c7bc92d5
nnsncl <nicolas.mgta@gmail.com>
2020-12-30 23:40:32 +0100

    add input component

---
+++ src/components/Input/styles/Input.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Label = styled.label`
+    font-size: 1.3rem;
+    font-weight: 600;
+    line-height: 1.3;
+    margin-bottom: 0.5rem;
+`;
+export const Field = styled.input`
+    margin-bottom: 2.3rem;
+    padding: 1.9rem 1.3rem;
+    border: 0.01rem solid #f5f3f4;
+    border-radius: 8px;
+    outline: none;
+    font-size: 1.6rem;
+    background: #f5f3f4;
+`;
